eaea550fee1ee3e62055016df21da7d620c9a4f9
Thanaporn <thanapornyaisomanung@gmail.com>
2021-12-06 15:47:26 +0700

    add function for cal and css

--- react-calculator-app/src/App.js
+++ react-calculator-app/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, UseEffect } from "react";
+
 
 function App() {
+  const [preState, setPerState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  const inputNum = (a) => {
+
+  };
+
+  const operatorType = (a) =>{
+
+  };
+
+  const equals = (a) =>{
+
+  };
+
+  const minusplus = () =>{
+
+  };
+
+  const percent = () =>{
+
+  };
+
+  const reset = () => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <div className = "sereen">
+          <div className="btn light-gray" onClick={reset}>AC</div>
+          <div className="btn light-gray" onClick={percent}>%</div>
+          <div className="btn light-gray" onClick={minusplus}>+/-</div>
+          <div className="btn orange" onClick={operatorType}>/</div>
+          <div className="btn" onClick={inputNum}>7</div>
+          <div className="btn" onClick={inputNum}>8</div>
+          <div className="btn" onClick={inputNum}>9</div>
+          <div className="btn orange" onClick={operatorType}>X</div>
+          <div className="btn" className={inputNum}>4</div> 
+          <div className="btn" className={inputNum}>5</div>
+          <div className="btn" className={inputNum}>6</div>
+          <div className="btn orange" onClick={operatorType}>+</div>
+          <div className="btn" onClick={inputNum}>1</div>
+          <div className="btn" onClick={inputNum}>2</div>
+          <div className="btn" onClick={inputNum}>3</div>
+          <div className="btn orange" onClick={operatorType}>-</div>
+          <div className="btn" onClick={inputNum}>0</div>
+          <div className="btn" onClick={inputNum}>.</div>
+          <div className="btn" onClick={equals}>=</div>
+
+
+        </div>
+      </div>
+      
     </div>
   );
 }
